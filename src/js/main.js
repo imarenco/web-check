162ebde7d00807d37f141f679c987b1c5e3a874b
imrenco <imrenco@MacBook-Air-de-imrenco.local>
2023-12-11 22:21:29 -0800

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,7 @@ async function updateValue(event) {
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-    <a href="/movie/index.html?movieId=${product.id}">
+    <a href="./movie/index.html?movieId=${product.id}">
       <img
         class="card_img"
         src="${product.image}"
