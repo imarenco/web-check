46c73fe057dc163911386d6090c5cdc9751d243c
imrenco <imrenco@MacBook-Air-de-imrenco.local>
2023-12-11 22:27:41 -0800

    Updates

--- assets/main-d3c05ca1.js
+++ assets/main-d3c05ca1.js
@@ -1,5 +1,5 @@
 import{g as n,f as c,s as r,r as m}from"./api-93959e8c.js";const o=document.querySelector("#search");o.addEventListener("input",l);async function l(e){var i;const a=((i=e==null?void 0:e.target)==null?void 0:i.value)||"",s=n(a),t=s||await c(a);s||r(a,t),g(t,"#list")}function f(e){return`<li class="product-card">
-    <a href="./movie/index.html?movieId=${e.id}">
+    <a href="/web-check/movie/index.html?movieId=${e.id}">
       <img
         class="card_img"
         src="${e.image}"
